92b46ccae2fd4b8a735d461771676453ab881824
Bensunny0619 <bensunny0619@gmail.com>
2022-11-09 08:14:54 +0100

    fonts and hover effects adjusted

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'],
+        sans: ['Basier Circle', 'sans-serif'],
       },
       colors: {
-        'background-white' : '#FFFFFF',
-        'main-black' : '#2A3F54',
-        'pastel-purple' : '#F0DCF8',
-        'main-yellow' : '#FAF5E2',
-        'sub-text' : '#8F9CA9',
+        'background-white': '#FFFFFF',
+        'main-black': '#2A3F54',
+        'pastel-purple': '#F0DCF8',
+        'main-yellow': '#FAF5E2',
+        'sub-text': '#8F9CA9',
       }
     },
   },
